f79365f8d00ee2bd09b42c7ecb3cdd658a88f40b
dlxla4820 <gorhfqhk@gmail.com>
2024-06-05 14:47:35 +0900

    init

---
+++ src/test/java/nicTry/nextClass/NextClassApplicationTests.java
@@ -0,0 +1,13 @@
+package nicTry.nextClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
