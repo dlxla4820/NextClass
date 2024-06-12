9ff6b8a5316e4bbbbc6874e4879b98373a35fb56
dlxla4820 <gorhfqhk@gmail.com>
2024-06-12 18:10:34 +0900

    ErrorCode 수정

--- src/main/java/com/nextClass/enums/ErrorCode.java
+++ src/main/java/com/nextClass/enums/ErrorCode.java
@@ -10,9 +10,9 @@ public enum ErrorCode {
     JSON_INVALID("E00101", "유효하지 않는 Json형식입니다."),
     REQUEST_BODY_NULL("E00102","요청본문이 비어있습니다."),
     PARAMETER_INVALID_GENERAL("E00103", "유효하지 않는 Parameter가 포함되어 있습니다."),
-    PARAMETER_INVALID_SPECIFIC("E00104", "%s이(가) 유효하지 않습니다."),
 
-    MEMBER_DUPLICATED("E00201","%s이(가) 중복되었습니다.");
+    PARAMETER_INVALID_SPECIFIC("E00201", "%s이(가) 유효하지 않습니다."),
+    MEMBER_DUPLICATED("E00202","%s이(가) 중복되었습니다.");
 
 
     private final String errorCode;
