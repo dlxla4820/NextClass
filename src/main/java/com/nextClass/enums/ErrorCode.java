603f391e4cc6fa2c099b9894038ccb5d4290ee21
dlxla4820 <gorhfqhk@gmail.com>
2024-07-02 17:21:57 +0900

    에러코드 수정

--- src/main/java/com/nextClass/enums/ErrorCode.java
+++ src/main/java/com/nextClass/enums/ErrorCode.java
@@ -16,10 +16,10 @@ public enum ErrorCode {
     JWT_ACCESS_EXPIRED_INVALID("E00106","만료된 Access 토큰입니다."),
     JWT_REFRESH_INVALID("E00107", "유효하지 않은 Refresh 토큰입니다."),
     JWT_REFRESH_EXPIRED_INVALID("E00108","만료된 Refresh 토큰입니다."),
+    PARAMETER_INVALID_SPECIFIC("E00109", "%s이(가) 유효하지 않습니다."),
+    INPUT_DUPLICATED("E00110","%s이(가) 중복되었습니다."),
 
-    PARAMETER_INVALID_SPECIFIC("E00201", "%s이(가) 유효하지 않습니다."),
-    MEMBER_DUPLICATED("E00202","%s이(가) 중복되었습니다."),
-    MEMBER_NOT_EXIST("E00203","Request에 해당하는 회원이 존재하지 않습니다.");
+    MEMBER_NOT_EXIST("E00201","Request에 해당하는 회원이 존재하지 않습니다.");
 
     private final String errorCode;
     private final String errorDescription;
