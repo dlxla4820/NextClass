ed7fe150ab5a2e99a2360ad049ed29cf3a3e1f1d
dlxla4820 <gorhfqhk@gmail.com>
2024-08-04 21:31:39 +0900

    camel로 변경

--- src/main/java/com/nextClass/dto/LoginRequestDto.java
+++ src/main/java/com/nextClass/dto/LoginRequestDto.java
@@ -1,5 +1,6 @@
 package com.nextClass.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,13 +13,14 @@ import java.util.StringJoiner;
 public class LoginRequestDto {
     private String id;
     private String password;
-    private String app_token;
+    @JsonProperty("app_token")
+    private String appToken;
     @Override
     public String toString() {
         return new StringJoiner(", ", LoginRequestDto.class.getSimpleName() + "[", "]")
                 .add("id='" + id + "'")
                 .add("password='" + password + "'")
-                .add("app_token='" + app_token + "'")
+                .add("appToken='" + appToken + "'")
                 .toString();
     }
 }
--- src/main/java/com/nextClass/dto/MemberChangeNormalInfoRequestDto.java
+++ src/main/java/com/nextClass/dto/MemberChangeNormalInfoRequestDto.java
@@ -1,5 +1,6 @@
 package com.nextClass.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,16 +13,16 @@ import java.util.StringJoiner;
 public class MemberChangeNormalInfoRequestDto {
 
     private String name;
-
-    private Integer member_grade;
-
-    private String member_school;
+    @JsonProperty("member_grade")
+    private Integer memberGrade;
+    @JsonProperty("member_school")
+    private String memberSchool;
     @Override
     public String toString() {
         return new StringJoiner(", ", MemberChangeNormalInfoRequestDto.class.getSimpleName() + "[", "]")
                 .add("name='" + name + "'")
-                .add("member_grade='" + member_grade + "'")
-                .add("member_school='" + member_school + "'")
+                .add("memberGrade='" + memberGrade + "'")
+                .add("memberSchool='" + memberSchool + "'")
                 .toString();
     }
 }
