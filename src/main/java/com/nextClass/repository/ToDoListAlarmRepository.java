f959f82c9ef902f6c44c497611114e5093a94e42
ChyoPyoRo <allen15590@naver.com>
2024-09-04 18:32:39 +0900

    alarm 한시간 마다 taskScheduler에 등록 및 코드 개선

--- src/main/java/com/nextClass/repository/ToDoListAlarmRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nextClass.repository;
-
-import com.nextClass.entity.ToDoListAlarm;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface ToDoListAlarmRepository extends CrudRepository<ToDoListAlarm, UUID> {
-}
